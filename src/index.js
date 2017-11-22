6dd9e028bd6c4063e0de32a7e368ddb228bb4a53
hirano-s <happy.hirano@gmail.com>
2017-11-22 11:55:51 +0900

    fix(webpack): added webpack support, refreshed build and package.json

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import { PLATFORM } from 'aurelia-pal';
 import LeafletCustomElement from './leaflet';
 
-export function configure(frameworkConfig) 
-    //LeafletDefaultImagePath = 'jspm_packages/npm/leaflet@1.0.2/dist/images/'
-//    LeafletDefaultImagePath = '../leaflet/dist/images/';
-    //  } = {}) {
-    
-//    Leaflet.Icon.Default.imagePath = LeafletDefaultImagePath;
-{
-//    Leaflet.Icon.Default.imagePath = null;
+export function configure(frameworkConfig) {
     frameworkConfig.globalResources(PLATFORM.moduleName('./leaflet'));
   }
 
