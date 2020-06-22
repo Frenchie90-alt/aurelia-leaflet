d84c7c6577c884bb1da18bdfa1e75ef362f17319
Frenchie90-alt <66291998+Frenchie90-alt@users.noreply.github.com>
2020-06-22 13:48:01 +1000

    Polygon was spelt incorrectly should be Polygon
    This spelling error causes a failure when the type is trying to be read by leaflet.

--- src/helpers/layer-factory.js
+++ src/helpers/layer-factory.js
@@ -38,11 +38,11 @@ export class LayerFactory {
       case 'multiPolyline':
         instance = this.getMultiPolyline(layer);
         break;
-      case 'polygone':
-        instance = this.getPolygone(layer);
+      case 'polygon':
+        instance = this.getPolygon(layer);
         break;
-      case 'multiPolygone':
-        instance = this.getMultiPolygone(layer);
+      case 'multiPolygon':
+        instance = this.getMultiPolygon(layer);
         break;
       case 'rectangle':
         instance = this.getRectangle(layer);
@@ -152,18 +152,18 @@ export class LayerFactory {
     return this.L.multiPolyline(layer.latlngs, layer.options);
   }
 
-  getPolygone(layer) {
+  getPolygon(layer) {
     if (!layer.hasOwnProperty('latLngs')) {
-      throw new AureliaLeafletException('No latLngs given for layer.type "polygone"');
+      throw new AureliaLeafletException('No latLngs given for layer.type "polygon"');
     }
-    return this.L.polygone(layer.latlngs, layer.options);
+    return this.L.polygon(layer.latlngs, layer.options);
   }
 
-  getMultiPolygone(layer) {
+  getMultiPolygon(layer) {
     if (!layer.hasOwnProperty('latLngs')) {
-      throw new AureliaLeafletException('No latLngs given for layer.type "multiPolygone"');
+      throw new AureliaLeafletException('No latLngs given for layer.type "multiPolygon"');
     }
-    return this.L.multiPolygone(layer.latlngs, layer.options);
+    return this.L.multiPolygon(layer.latlngs, layer.options);
   }
 
   getRectangle(layer) {
